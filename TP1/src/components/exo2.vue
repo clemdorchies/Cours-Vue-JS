762a08fc17320b03836aee60855529392a997598
Clément <clem.dorchies@gmail.com>
2022-03-01 15:30:33 +0100

    New TP2

--- TP1/src/components/exo2.vue
+++ TP1/src/components/exo2.vue
@@ -1,30 +1,43 @@
+<template>
+    <form v-if="isFormActive">
+        Nom :
+        <input type="text" v_model="nom" />
+        Prénom :
+        <input type="text" v_model="prenom" />
+        Date de naissance :
+        <input type="text" v_model="ddn" />
+        Mail :
+        <input type="text" v_model="mail" />
+        <button @click="valider">Valider</button>
+    </form>
+    <div v-else>Félicitation, bravo, blabla !</div>
+</template>
+
 <script>
 import { defineComponent } from 'vue';
 
 export default defineComponent({
     data: () => {
         return {
-            nom: '',
-            prenom: '',
-            ddn: '',
-            mail: '',
+            nom: null,
+            prenom: null,
+            ddn: null,
+            mail: null,
+            isFormActive: true,
         }
     },
+    methods: {
+        valider() {
+            if (this.nom && this.prenom && this.ddn && this.mail) {
+                this.isFormActive = false;
+            }
+        }
+    }
 });
 </script>
 
-<template>
-    <form action="" class="exo2">
-        Nom : <input type="text" v_model="nom" />
-        Prénom : <input type="text" v_model="prenom" />
-        Date de naissance : <input type="text" v_model="ddn" />
-        Mail : <input type="text" v_model="mail" />
-        <button @click="valider()">Valider</button>
-    </form>
-</template>
-
 <style>
 .exo2 {
-    margin-bottom : 50px;
+    margin-bottom: 50px;
 }
 </style>
